998bd1a3fcf6716be058942b3299ac35cfaca71e
MANNE UDAY KIRAN <143863214+ManneUdayKiran@users.noreply.github.com>
2025-08-01 19:51:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-     allowedHosts: ['https://aistudylab.onrender.com','https://a-istudy-lab-of1b.vercel.app/api/auth/register','https://aistudylab-1.onrender.com'],
+     allowedHosts: ['aistudylab-1.onrender.com','https://aistudylab.onrender.com','https://a-istudy-lab-of1b.vercel.app/api/auth/register','https://aistudylab-1.onrender.com'],
   },
 })
